7e95f11ea987257d1c481d33515b73ad6ddd1f94
Natielle Arruda <78966664+NatielleArruda@users.noreply.github.com>
2022-01-20 13:42:47 -0300

    Inclusão do Tema

--- src/main/java/br/org/generation/blogpessoal/model/Postagem.java
+++ src/main/java/br/org/generation/blogpessoal/model/Postagem.java
@@ -6,30 +6,45 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @Entity
-@Table(name="tb_postagens") // create table tb_postagens(
+@Table(name = "tb_postagens") // create table tb_postagens(
 public class Postagem {
-	
+
 	@Id // primary key (id)
-	@GeneratedValue(strategy = GenerationType.IDENTITY) // id auto increment 
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // id auto increment
 	private Long id;
-	
+
 	@NotBlank(message = "O artributo título é obrigatório!")
 	@Size(min = 5, max = 100, message = "O atributo título deve conter no mínimo 5 e no máximo 100 caracteres!")
-	private String titulo;	
-	
+	private String titulo;
+
 	@NotBlank(message = "O artributo texto é obrigatório!")
 	@Size(min = 10, max = 1000, message = "O atributo texto deve conter no mínimo 10 e no máximo 1000 caracteres!")
 	private String texto;
-	
+
 	@UpdateTimestamp
 	private LocalDateTime data;
+
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
 
 	public Long getId() {
 		return id;
@@ -64,4 +79,3 @@ public class Postagem {
 	}
 
 }
-
